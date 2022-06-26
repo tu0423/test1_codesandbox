09bba7a28826e9b720f14303623b538023767d02
宇川徹 <54793973+tu0423@users.noreply.github.com>
2022-06-26 10:00:16 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -16,9 +16,25 @@ const onClickAdd = () => {
   //button完了タグ作成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
+  console.log(completeButton);
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button削除タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  console.log(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   //divの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
